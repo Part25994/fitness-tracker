6197f269a53a41d12d203c670dd90f06bedc04a3
Ocawesome101 <ocawesome101@gmail.com>
2020-11-07 23:35:54 -0500

    rework ui.js

---
+++ challenge_preset.js
@@ -0,0 +1,3 @@
+let layouts = {
+  1: {}
+}
--- ui.js
+++ ui.js
@@ -1,27 +1,15 @@
 // YOU EYE DOT JAY ESS
 
-let elements = {}
-let bases = {}
-
-// element definitions
-bases.button = {
-  onclick: function() {},
-  draw: function() {
+let elements = document.getElementById('ui-root');
+let presets = {
+  challenge: {
+    1: {},
+    setup: function() {
+    }
   }
 }
 
-function Element(name) {
-  this.children = {};
-  this.addChild = function(obj) {
-    this.children[obj] = true;
-  }
-  for (method in bases[name]) {
-    this[method] = bases[name][method];
-  }
-}
-
-function newObject(name, pnt) {
-  var nobj = new Element(name);
-  pnt.addChild(nobj)
-  return nobj;
+function loadPreset(pst) {
+  let preset = presets[pst]
+  preset.setup(elements)
 }
