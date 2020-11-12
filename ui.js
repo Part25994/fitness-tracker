d20e529c006e78e5d0bcaa332169396e11dff9cf
Ocawesome101 <ocawesome101@gmail.com>
2020-11-11 19:14:52 -0500

    we get to stage II now

--- ui.js
+++ ui.js
@@ -11,24 +11,28 @@ let presets = {
     `,
     // stage 2: challenge selection
     2: `
- 
      <li><button onclick="challenge">Most pushups in a minute</button></li>
      <li><button onclick="challenge">Most situps in a minute</button></li>
      <li><button onclick="challenge">Most steps in a day</button></li>
      <li><button onclick="challenge">most steps in a week</button></li>
      <li><button onclick="challenge">Most burpees in a minute</button></li>
      <input type="text">Other</input>
-`,
+    `,
     // stage 3: confirmation
     3: ``,
     setup: function() {
       this.i = 1
       let root = document.getElementById("ui-root")
+      let obj = this
       root.innerHTML = ""
       root.innerHTML = this[this.i]
       let onclick = function() {
-        
+        obj.i = obj.i + 1
+        root.innerHTML = obj[obj.i]
       }
+      root.children[1].onclick = onclick
+      root.children[2].onclick = onclick
+      root.children[3].onclick = onclick
     }
   }
 }
